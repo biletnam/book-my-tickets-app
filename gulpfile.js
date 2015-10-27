9bffd382cfb426331b64e617861ac62a27cbf1cf
deepak6sp <deepak6sp@gmail.com>
2015-10-27 18:18:28 +1100

    master changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,10 @@ var rename   	= require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 
+
 gulp.task('sass', function () {
   gulp.src('./sass-styles/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./minifycss'));
-});
-
-gulp.task('minify-css', function () {
-    gulp.src('./minifycss/*.css') // path to your file
     .pipe(minifyCss())
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('./minifycss'));
@@ -23,12 +19,13 @@ gulp.task('watch', function () {
   gulp.watch('./sass-styles/main.scss',['sass']);
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync',['watch'],function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
+    gulp.watch("./**/*").on('change', browserSync.reload);
 });
 
-gulp.task('default',['watch']);
+gulp.task('default',['browser-sync']);
